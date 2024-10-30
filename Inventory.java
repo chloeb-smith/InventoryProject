c2807e211cf03fcacc73a0d622496178be143f33
chloeb-smith <158786659+chloeb-smith@users.noreply.github.com>
2024-10-30 14:20:14 -0400

    Added Inventory class file and main class file

---
+++ Inventory.java
@@ -0,0 +1,10 @@
+public class Inventory {
+
+    Apparel[] apparelItems;
+
+    Inventory(Apparel[] apparelItems){
+        this.apparelItems = apparelItems;
+
+    }
+    public void addItem(Apparel item){apparelItems}
+}

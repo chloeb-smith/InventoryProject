4554c2c4a8dd77b5aa176a723d2ef9fa871a9dd9
tarheelfan200 <tarheelfan3951@gmail.com>
2024-11-04 13:14:29 -0500

    update

--- Main.java
+++ Main.java
@@ -4,17 +4,20 @@ public class Main {
     public static void main(String[] args) {
 
         ArrayList<Apparel> apparelItems = new ArrayList<>();
+        // create inventory object - storing apparel items
+        Inventory inventory = new Inventory(apparelItems);
 
         Apparel gucciShirt = new Apparel("Gucci Shirt", "10/1/12", "L", "Top");
         Apparel basicShirt = new Apparel("Basic Shirt", "1/10/19", "M", "Top");
         Apparel watchShirt = new Apparel("Watch Shirt", "8/15/82", "S", "Top");
 
-        apparelItems.add(gucciShirt);
-        apparelItems.add(basicShirt);
-        apparelItems.add(watchShirt);
+        //apparelItems.add(gucciShirt);
+        //apparelItems.add(basicShirt);
+        //apparelItems.add(watchShirt);
 
-        Inventory inventory = new Inventory(apparelItems);
-        inventory.displayApparelItems();
+        inventory.addItem(gucciShirt);
+        inventory.addItem(basicShirt);
+        inventory.addItem(watchShirt);
 
         System.out.print(inventory.displayApparelItems());
     }
